a8a84cb11ff00b0baf8d5ae855abb1b9dd89b5b3
AntonShevchuk <AntonShevchuk@7659c917-e742-0410-a2bd-ff856f703c97>
2008-04-15 11:24:28 +0000

    add Zend Framework support

--- javascript/jquery.php.js
+++ javascript/jquery.php.js
@@ -242,3 +242,37 @@ php = {
     }
 }
 // end of php actions
+
+// example php extension to jQuery (example)
+jQuery.extend({
+    php: function (url, params) {        
+        // do an ajax post request
+        $.ajax({
+           // AJAX-specified URL
+           url: url,
+           // JSON
+           type: "POST",
+           data: params,
+           dataType : "json",
+           
+           /* Handlers */
+           
+           // Handle the beforeSend event
+           beforeSend: function(){
+               return php.beforeSend();
+           },
+           // Handle the success event
+           success: function(data, textStatus){
+               return php.success(data, textStatus);
+           },
+           // Handle the error event
+           error: function (xmlEr, typeEr, except) {
+               return php.error(xmlEr, typeEr, except);                  
+           },
+           // Handle the complete event
+           complete: function (XMLHttpRequest, textStatus) {              
+               return php.complete(XMLHttpRequest, textStatus);
+           }
+        })
+    }
+});
